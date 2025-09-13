5eb1a09f6463bc52e0b5ce2839b21a37aa3a71bd
Philippe Mourey <philippe.mourey@gmail.com>
2025-09-14 00:46:01 +0200

    version 1.0

--- app.pl
+++ app.pl
@@ -5,6 +5,7 @@ use warnings FATAL => 'all';
 use Dancer2;
 use lib 'lib';
 use ContactManager;
+use POSIX;
 
 # Home page
 get '/' => sub {
@@ -85,19 +86,140 @@ post '/modifier' => sub {
 };
 
 # Backup contacts
-get '/backup' => sub {
+get '/backup_old' => sub {
     my $backup_file = ContactManager::backup_contacts();
-    return "Backup créé avec succès : $backup_file";
+    return template 'backup' => {
+        current_page => 'backup',
+        backup_file  => $backup_file,
+    };
 };
 
-# Restore contacts (pass ?file=backupname.csv)
-get '/restore' => sub {
-    my $file = query_parameters->get('file');
-    if ($file) {
-        ContactManager::restore_contacts($file);
-        return "Restauration effectuée depuis : $file";
+get '/backup' => sub {
+    my $dir = 'backups';
+    mkdir $dir unless -d $dir;  # crée le dossier s'il n'existe pas
+
+    # Générer un timestamp YYYYMMDD_HHMMSS
+    my $timestamp = strftime "%Y%m%d_%H%M%S", localtime;
+    my $backup_file = "$dir/contacts_backup_$timestamp.csv";
+
+    # Lire contacts existants et écrire le backup
+    if (-e 'contacts.csv') {
+        open my $in, '<', 'contacts.csv' or return "Impossible de lire contacts.csv : $!";
+        open my $out, '>', $backup_file or return "Impossible de créer $backup_file : $!";
+        while (my $line = <$in>) {
+            print $out $line;
+        }
+        close $in;
+        close $out;
+    } else {
+        return "Aucun contact à sauvegarder.";
     }
-    return "Fichier de backup non spécifié.";
+
+    # Retourne template avec confirmation
+    template 'backup' => {
+        current_page => 'backup',
+        backup_file  => $backup_file,
+    };
 };
+
+
+get '/restore_old' => sub {
+    my $dir = 'backups';
+    opendir(my $dh, $dir) or return "Impossible d'ouvrir $dir: $!";
+    my @files = grep { /\.csv$/ && -f "$dir/$_" } readdir($dh);
+    closedir $dh;
+
+    return template 'restore' => {
+        current_page => 'restore',
+        backups      => \@files,
+    };
+};
+
+use POSIX 'strftime';
+
+get '/restore' => sub {
+    my $dir = 'backups';
+    opendir(my $dh, $dir) or return "Impossible d'ouvrir $dir: $!";
+    my @files = grep { /\.csv$/ && -f "$dir/$_" } readdir($dh);
+    closedir $dh;
+
+    # Décoder le timestamp dans le nom du fichier
+    my @backups;
+    foreach my $file (@files) {
+        if ($file =~ /_(\d{8})_(\d{6})\.csv$/) {
+            my ($date, $time) = ($1, $2);
+            my $formatted_date = sprintf "%s-%s-%s %s:%s:%s",
+                substr($date,0,4), substr($date,4,2), substr($date,6,2),
+                substr($time,0,2), substr($time,2,2), substr($time,4,2);
+            push @backups, { file => $file, date => $formatted_date };
+        } else {
+            push @backups, { file => $file, date => 'Inconnue' };
+        }
+    }
+
+    # Tri par date descendante (optionnel)
+    @backups = sort { $b->{date} cmp $a->{date} } @backups;
+
+    template 'restore' => {
+        current_page => 'restore',
+        backups      => \@backups,
+    };
+};
+
+# Exécution de la restauration (via paramètre ?file=)
+get '/do_restore' => sub {
+    my $file = query_parameters->get('file');
+    my $msg;
+
+    if ($file) {
+        ContactManager::restore_contacts("backups/$file");
+        $msg = "Restauration effectuée depuis : $file";
+    } else {
+        $msg = "Fichier de backup non spécifié.";
+    }
+
+    return template 'do_template' => {
+        current_page => 'restore',
+        message      => $msg,
+    };
+};
+
+# Supprimer un backup
+post '/delete_backup' => sub {
+    my $file = body_parameters->get('file');
+    my $msg;
+
+    if ($file && -f "backups/$file") {
+        unlink "backups/$file" or $msg = "Impossible de supprimer $file : $!";
+        $msg ||= "Backup supprimé : $file";
+    } else {
+        $msg = "Fichier de backup non spécifié ou inexistant.";
+    }
+
+    return template 'do_template' => {
+        current_page => 'restore',
+        message      => $msg,
+    };
+};
+
+get '/contacts' => sub {
+    my @contacts;
+
+    if (-e 'contacts.csv') {
+        open my $fh, '<', 'contacts.csv' or die "Impossible d'ouvrir contacts.csv : $!";
+        while (my $ligne = <$fh>) {
+            chomp $ligne;
+            my ($nom, $email, $tel) = split /,/, $ligne;
+            push @contacts, { nom => $nom, email => $email, tel => $tel };
+        }
+        close $fh;
+    }
+
+    template 'contacts' => {
+        current_page => 'contacts',
+        contacts     => \@contacts,
+    };
+};
+
 
 start;
